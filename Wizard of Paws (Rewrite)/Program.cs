39484a2ed034a05a133e20fc96c7fbfbb2e0a426
LeifTheLion <furrymau5@gmail.com>
2018-11-08 21:31:54 -0600

    Add project files.

---
+++ Wizard of Paws (Rewrite)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wizard_of_Paws__Rewrite_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
