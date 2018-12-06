05364f9684f6df2ed2f0b8354bd1420c9dab5e9f
LeifTheLion <furrymau5@gmail.com>
2018-12-06 00:22:58 -0600

    added unmute command

--- Wizard of Paws (Rewrite)/Moderation.cs
+++ Wizard of Paws (Rewrite)/Moderation.cs
@@ -13,17 +13,16 @@ namespace WizardBot
 {
     public class Moderation : ModuleBase<SocketCommandContext>
     {
-        [Group("mute")]
         public class MuteModule : ModuleBase<SocketCommandContext>
         {
-            [Command("channelmute")]
-            [Alias("channel", "c")]
-            [Summary("Creates a new role.")]
+            [Command("mute")]
+            [Alias("muzzle")]
+            [Summary("Mutes the user.")]
             [RequireBotPermission(GuildPermission.ManageRoles)]
             [RequireUserPermission(GuildPermission.Administrator)]
             public async Task MuteTask(SocketGuildUser socketGuildUser, string forTime = null)
             {
-                //Checks if message is from me
+                //Checks if user context is owner
                 if (socketGuildUser == Context.Guild.GetUser(174768008529575936))
                 {
                     await ReplyAsync("I cannot mute my creator.");
@@ -33,20 +32,18 @@ namespace WizardBot
                 //Admin Check
                 if (PermissionHelper.HasPermission(socketGuildUser, GuildPermission.Administrator))
                 {
-                    await ReplyAsync($"{socketGuildUser.Username} is an Administrator and cannot be muted.");
+                    await ReplyAsync($"{socketGuildUser.Mention} is untameable, put your muzzle away.");
                     return;
                 }
 
                 //Fetching the Muted Role
                 SocketRole mutedRole =
-                    (from r in socketGuildUser.Guild.Roles where r.Name == "Muted" select r)
-                    .FirstOrDefault();
+                    (from r in socketGuildUser.Guild.Roles where r.Name == "Muted" select r).FirstOrDefault();
                 //Create the role if it doesn't exist.
                 if (mutedRole == null)
                 {
                     await socketGuildUser.Guild.CreateRoleAsync("Muted", GuildPermissions.None, Color.DarkRed, true);
-                    mutedRole = (from r in socketGuildUser.Guild.Roles where r.Name == "Muted" select r)
-                        .FirstOrDefault();
+                    mutedRole = (from r in socketGuildUser.Guild.Roles where r.Name == "Muted" select r).FirstOrDefault();
                 }
 
                 //Add the role to user
@@ -55,7 +52,48 @@ namespace WizardBot
                 //Add role permissions to the channel
                 var perms = new OverwritePermissions(sendMessages: PermValue.Deny);
                 await socketGuildUser.Guild.GetTextChannel(Context.Channel.Id).AddPermissionOverwriteAsync(mutedRole, perms);
-                await ReplyAsync($"{socketGuildUser.Mention} has been muzzled in this channel.");
+                await ReplyAsync($"{socketGuildUser.Mention} has been muzzled.");
+            }
+        }
+
+        public class UnmuteModule : ModuleBase<SocketCommandContext>
+        { 
+            [Command("unmute")]
+            [Alias("unmuzzle")]
+            [Summary("Unmutes a user.")]
+            [RequireBotPermission(GuildPermission.ManageRoles)]
+            [RequireUserPermission(GuildPermission.Administrator)]
+            public async Task UnmuteTask(SocketGuildUser socketGuildUser, string forTime = null)
+            {
+                //Checks if user context is owner
+                if (socketGuildUser == Context.Guild.GetUser(174768008529575936))
+                {
+                    await ReplyAsync("I will not disobey my master.");
+                    return;
+                }
+
+                //Admin role check
+                if (PermissionHelper.HasPermission(socketGuildUser, GuildPermission.Administrator))
+                {
+                    await ReplyAsync($"{socketGuildUser.Mention} is untameable, put your muzzle away.");
+                    return;
+                }
+
+                //Fetches role
+                SocketRole mutedRole =
+                    (from r in socketGuildUser.Guild.Roles where r.Name == "Muted" select r).FirstOrDefault();
+
+                //Checks user for role and unmutes them
+                if (PermissionHelper.IsUserRoleHolder(socketGuildUser, mutedRole.Name))
+                {
+                    await socketGuildUser.RemoveRoleAsync(mutedRole);
+                    await ReplyAsync($"{socketGuildUser.Mention}, {Context.User.Username} has removed your muzzle.");
+                    return;
+                }
+
+                await ReplyAsync($"{socketGuildUser.Nickname} has not been muzzled.");
+                return;
+                
             }
         }
     }
